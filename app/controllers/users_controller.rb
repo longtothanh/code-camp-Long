dbb9bd7e7c224559df5695fac135331e36691a42
longtothanh <longtt@zigexn.vn>
2024-12-19 19:14:29 +0700

    render notifications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,20 @@ class UsersController < ApplicationController
 
     def index
         @users = User.where.not(id: current_user.id)
-        @notifications = Notification.where(notifiable_id: current_user.id)
-        # binding.pry
+        # @notifications = Notification.where(notifiable_id: current_user.id).joins(:user).select("")
+        @notifications = Notification.joins(:user).where(notifiable_id: current_user.id).select('notifications.*, users.email AS email').map do |notification|
+            {
+                id: notification.id,
+                user_id: notification.user_id,
+                notifiable_type: notification.notifiable_type,
+                notifiable_id: notification.notifiable_id,
+                action_type: notification.action_type,
+                created_at: notification.created_at,
+                updated_at: notification.updated_at,
+                email: notification.email,
+                message: "#{notification.email} đã #{notification.action_type}"
+            }
+        end
     end
 
     def show
